125191158ad201c876512ef8bdf9ac80231e66ae
Stephanie Roe <stephanie.j.roe@gmail.com>
2022-02-05 12:56:38 -0700

    Add shelf test

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,43 @@
 
+function shelfBook(book, sciFiShelf) {
+    if (sciFiShelf.length < 3) {
+      return sciFiShelf.unshift(book)
+    } else {
+      return
+    }
+};
+
+function unshelfBook(book, sciFiShelf) {
+  for (var i = 0; i < sciFiShelf.length; i++) {
+    if (sciFiShelf[i].title === book) {
+    return sciFiShelf.splice(i, 1)
+  }
+}
+};
+
+var newShelf = [];
+
+function listTitles(fantasyShelf) {
+  for (var i = 0; i < fantasyShelf.length; i++) {
+    newShelf.push(fantasyShelf[i].title)
+  }
+    return newShelf.join(", ")
+};
+
+function searchShelf(sciFiShelf, book) {
+  for (var i = 0; i < sciFiShelf.length; i++) {
+    if (sciFiShelf[i].title === book) {
+      return true
+    }
+  }
+      return false
+
+  };
+
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
